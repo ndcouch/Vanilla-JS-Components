062e9c5415ceb4e9262d718a6e193806bd9e594f
Nate Couch <ndcouch@gmail.com>
2019-10-11 04:18:02 -0400

    Added book list, modified sandbox

--- JS_sandbox/app.js
+++ JS_sandbox/app.js
@@ -1,3 +1,19 @@
-localStorage.setItem("name", "nate");
+// localStorage.setItem("name", "nate");
 
-sessionStorage.setItem("name", "diana");
+// sessionStorage.setItem("name", "diana");
+
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName - lastName;
+  this.birthday = new Date(dob);
+  this.calculateAge = function() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  };
+}
+
+const nate = new Person("Nate", "Couch", "9-19-81");
+const diana = new Person("Diana", "McGuire", "8/11/83");
+
+console.log(diana);
